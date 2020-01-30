1e7b286ece0f73f9b4d5a4ccb3d9f7c4d446a982
Prasanjit Rath <48017720+Avenge-PRC777@users.noreply.github.com>
2020-01-30 22:13:15 +0530

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # inbrowser-tank-game
 This is a real time tank game using Websockets!
+
+# Learning about a server!
+
+## What is Routing?
+## What are types of HTTP requests?
+## How to make HTTP requests in linux?
+## What are static files?
---
+++ server.js
@@ -0,0 +1,30 @@
+var express=require('express');
+var http=require('http');//This is a standard http server code 
+//with express as defacto server side code
+
+var path=require('path');
+var socketIO=require('socket.io');
+
+var app=express();//require('module.js')(.js can be omitted) 
+//returns whatever is exported by that module.js as module.exports=
+//an object or function or string
+//So, express returns/exports a function to variable express, and app
+//calls it
+
+const port=5000;
+//http.Server.listen(port) is same as app.listen(port)
+//app is to be given to node's HTTP servers as callback function
+
+var server=http.Server(app);
+
+var io=socketIO(server);app.set('port',port);
+
+app.use(express.static('public'));//Middleware, called before sending response
+
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname,'index.html'));
+});
+
+server.listen(port,()=>{
+	console.log(`Starting server on port ${port}`);
+});
